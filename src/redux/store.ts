b3e6def8b8ec4703c32b26196a86b811c6d18859
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-03-05 02:08:22 +0200

    add persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit"
 import tasksReducer from './task/tasksSlice'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
 
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['tasks'], 
+}
+
+
+const persistedReducer = persistReducer(persistConfig, tasksReducer)
 
 
 export const store = configureStore({
   reducer: {
-    tasks: tasksReducer,
+    tasks: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  
   devTools: process.env.NODE_ENV === 'development',
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
