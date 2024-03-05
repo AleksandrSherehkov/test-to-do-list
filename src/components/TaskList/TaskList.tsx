d05f71be5a499097c1d71dbd582ecce7c355830e
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-03-05 16:34:39 +0200

    add animation

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,4 +1,6 @@
 
+import { AnimatePresence, motion } from 'framer-motion'
+
 import { TaskListItem } from '../TaskListItem/TaskListItem'
 import { Task } from '../../utils/definitions'
 
@@ -14,19 +16,29 @@ interface TaskListProps {
 
 export const TaskList: React.FC<TaskListProps> = ({ tasks, toggleTask, editTask, deleteTask }) => {
   return (
-    <div className=' flex flex-col it custom-scrollbar w-full pr-2'>
-      <ul className='flex flex-col gap-y-3 w-full px-2'>
-        {tasks.map((task, index) => 
-          <TaskListItem
-            key={task.id}
-            task={task}
-            index={index}
-            toggleTask={toggleTask}
-            editTask={editTask}
-            deleteTask={deleteTask}
-          />
-        )}
-      </ul>
+    <div className='flex flex-col it custom-scrollbar w-full pr-2'>
+      <AnimatePresence>
+        <ul className='flex flex-col gap-y-3 w-full px-2'>
+          {tasks.map((task, index) => 
+            <motion.li
+              key={task.id}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20, transition: { duration: 0.2 } }}
+              transition={{ duration: 0.5 }}
+              layout
+            >
+              <TaskListItem
+                task={task}
+                index={index}
+                toggleTask={toggleTask}
+                editTask={editTask}
+                deleteTask={deleteTask}
+              />
+            </motion.li>
+          )}
+        </ul>
+      </AnimatePresence>
     </div>
   )
 }
