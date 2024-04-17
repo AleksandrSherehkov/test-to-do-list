be9b0e045fdef8f83310c7c5eff0dd1e30b7fc32
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-04-18 01:43:26 +0300

    Update TaskListItem.tsx

--- src/components/TaskListItem/TaskListItem.tsx
+++ src/components/TaskListItem/TaskListItem.tsx
@@ -21,26 +21,29 @@ export const TaskListItem: React.FC<TaskListItemProps> = ({
   index,
 }) => {
   return (
-    <li className="w-full flex justify-between items-center p-4 rounded-xl bg-fogWhite w-full shadow-xl transition-all duration-200 ease-in-out transform hover:scale-[102%] hover:bg-slate-300">
+    <li className="w-full flex justify-between items-center p-4 rounded-xl bg-fogWhite shadow-xl transition-all duration-200 ease-in-out transform hover:scale-[102%] hover:bg-slate-300">
       <button
         onClick={() => toggleTask(task.id)}
         className={`whitespace-pre-wrap text-left w-full cursor-pointer ${
-          task.completed ? 'text-red-500 line-through ' : 'text-swamp'
-        }`}
+          task.completed ? 'text-red-500 line-through' : 'text-swamp'
+        } transition duration-300 ease-in-out`}
       >
         {index + 1}. {task.name}
       </button>
       <div className="flex gap-3">
-        <button onClick={() => editTask(task.id)}>
+        <button
+          onClick={() => editTask(task.id)}
+          className="transition duration-200 ease-in-out"
+        >
           <BiSolidEditAlt
             size={24}
-            className="cursor-pointer fill-pastelGreen hover:fill-greenBright"
+            className="cursor-pointer fill-pastelGreen transition-all duration-300  hover:fill-greenBright"
           />
         </button>
         <button onClick={() => deleteTask(task.id)}>
           <MdDeleteForever
             size={24}
-            className="cursor-pointer fill-azure hover:fill-redDark"
+            className="cursor-pointer fill-azure transition-all duration-300  hover:fill-redDark"
           />
         </button>
       </div>
